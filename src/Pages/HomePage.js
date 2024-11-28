8a08f9a85218dfe753725fa880f6e6e20948cb21
jayaprakash802 <136257791+jayaprakash802@users.noreply.github.com>
2024-11-28 23:45:11 +0530

    Added User Authentication

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,10 @@
+
+const HomePage = () =>{
+    return(
+        <div>
+            <h1>Welcome to Our Store</h1>
+            <p>Explore our collection of products.</p>
+        </div>
+    );
+};
+export default HomePage;
---
+++ src/Pages/SignupPage.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+  const [form, setForm] = useState({ name: "", password: "", email: "" });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8090/api/auth/register", form);
+      
+      if (response.status === 201) {
+        alert("Registration successful! Navigating to login page");
+        navigate("/login"); // Redirect to login page
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Registration failed.");
+    }
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form">
+        <h2>Register</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
